dac92f24cf82f598d86963d06e8fc4560601dde0
landimc8 <89792005+landimc8@users.noreply.github.com>
2023-04-06 11:12:41 +0200

    Update server.R

--- TSP/server.R
+++ TSP/server.R
@@ -1,31 +1,79 @@
 
-library(shiny)
 
-# Define server logic required to draw a histogram
-
-server <- function(input, output) {
+# Define server
+# Define server
+server <- function(input, output, session) {
   
+  # Initialize map
   output$map <- renderLeaflet({
     leaflet() %>%
-      addProviderTiles(providers$OpenStreetMap)
+      addTiles() %>%
+      setView(lng = 0, lat = 0, zoom = 2)
   })
   
-  observeEvent(input$addMarkers, {
-    # Somewhere in London
-    randPoint1 <- runif(1, min=-0.34, max=0.16)
-    randPoint2 <- runif(1, min=51.34, max=51.67)
-    leafletProxy("map") %>%
-      addMarkers(lng=randPoint1, lat=randPoint2) %>%
-      setView(lng=randPoint1, lat=randPoint2, zoom=10)
+  # Add marker to map based on user selection
+  observe({
+    if(input$location == "USA"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -95.7129, lat = 37.0902,
+                   popup = "United States")
+    } else if(input$location == "Canada"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -106.3468, lat = 56.1304,
+                   popup = "Canada")
+    } else if(input$location == "Mexico"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -102.5528, lat = 23.6345,
+                   popup = "Mexico")
+    } else if(input$location == "Brazil"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -51.9253, lat = -14.235,
+                   popup = "Brazil")
+    } else if(input$location == "Argentina"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -63.6167, lat = -38.4161,
+                   popup = "Argentina")
+    } else if(input$location == "UK"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -0.1278, lat = 51.5074,
+                   popup = "United Kingdom")
+    } else if(input$location == "France"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = 2.3522, lat = 48.8566,
+                   popup = "France")
+    } else if(input$location == "Germany"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = 10.4515, lat = 51.1657,
+                   popup = "Germany")
+    } else if(input$location == "Italy"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = 12.5674, lat = 41.8719,
+                   popup = "Italy")
+    } else if(input$location == "Spain"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = -3.7492, lat = 40.4637,
+                   popup = "Spain")
+    } else if(input$location == "China"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = 104.1954, lat = 35.8617,
+                   popup = "China")
+    } else if(input$location == "Japan"){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        addMarkers(lng = 139.6503, lat = 35.6762,
+                   popup = "Japan")
+    }
   })
-  
-  observeEvent(input$saveMapUsingID, {
-    mapshot("map", file=paste0(getwd(), '/exported_map.png'))
-  })
-  
-  observeEvent(input$saveMapUsingProxy, {
-    m <- leafletProxy("map")
-    mapshot(m, file=paste0(getwd(), '/exported_map.png'))
-  })
-  
-}
+  }
+
